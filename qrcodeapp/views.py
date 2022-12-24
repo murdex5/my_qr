b6f90255674fde56e2bf75a02c66b4934a36f440
murdex5 <fkadupiige@gmail.com>
2022-12-24 20:57:32 +0100

    added logic to genareate qrcodes

--- qrcodeapp/views.py
+++ qrcodeapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.conf import settings
+from qrcode import *
+import time
 # Create your views here.
+
+def qr_gen(request):
+    if request.method == 'POST':
+        data = request.POST['data']
+        img = make(data)
+        img_name = 'qr' + str(time.time()) + '.png'
+        img.save(settings.MEDIA_ROOT + '/' + img_name)
+        context = {
+            'img_name': img_name
+            }
+        return render(request, 'index.html', context)
+    return render(request, 'index.html')
